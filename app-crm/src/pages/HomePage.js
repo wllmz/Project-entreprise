2861ed67f035cebcdbf581c581995d51968e8df3
wllmz <115452984+wllmz@users.noreply.github.com>
2024-05-30 12:13:52 +0200

    fixe features popup

--- app-crm/src/pages/HomePage.js
+++ app-crm/src/pages/HomePage.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 const HomePage = () => {
+  const { authState } = useContext(AuthContext);
+
+  if (!authState || !authState.user) {
+    return  <div className="text-center mt-10"><h1 className="text-4xl">Welcome to the Home Page</h1></div>;
+  }
+
   return (
     <div className="text-center mt-10">
-      <h1 className="text-4xl">Welcome to the Home Page</h1>
+      <h1 className="text-4xl">Welcome, {authState.user.username}!</h1>
     </div>
+    
   );
 };
 
